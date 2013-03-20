45b5d78bb72f7b90c67d8d44ace7be1208dec949
André Noack <noackorama@googlemail.com>
2013-03-20 22:57:51 +0100

    Update test.php

--- snipp/test.php
+++ snipp/test.php
@@ -1,5 +1,8 @@
-$a = array(1,3,5,7);
-$filter = create_function('$v', 'return $v > 3;');
-var_dump(array_filter($a, $filter));
-$filter = function($v){ return $v > 3;};
-var_dump(array_filter($a, $filter));
+class seminare extends SimpleORMap
+{
+}
+$s = new Seminare($id);
+echo $s->getValue('name');
+//ab 2.0
+echo $s->name;
+echo $s['name'];
