8f1c85adecf5136c7c816e65ab3be223adfc1b8b
André Noack <noackorama@googlemail.com>
2013-03-21 23:43:40 +0100

    Update test.php

--- snipp/test.php
+++ snipp/test.php
@@ -1,6 +1,14 @@
 $course = Course::find($id);
-$course->delete();
-if ($course->isDeleted()) {
-  $course->setNew();
-  $course->store();
-}
+$course->members[0]->user_id;
+$course->members->val('user_id');
+//findBy() liefert Collection
+$course->members->findBy('status', 'dozent')->pluck('user_id');
+$course->members->findBy('status', 'dozent')->toGroupedArray('user_id', 'username vorname nachname');
+unset($course->members[0]); //geht zwar
+$course->member->unsetBy('username', 'noack'); //sinnvoller
+$course->member->getDeleted();
+$course->member->refresh();
+
+$courses_collection = SimpleOrMapCollection::createFromArray(Course::findbySQL("name LIKE ?", array('Test%')));
+$courses_collection->each(function($c) {$c->visible = 0; $c->store();});
+$courses = $courses_collection->pluck('name');
