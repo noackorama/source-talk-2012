ff08f6dd00a5d3ba0930dc0a48cfe51c8aeeaa13
André Noack <noackorama@googlemail.com>
2013-03-21 15:05:57 +0100

    Update test.php

--- snipp/test.php
+++ snipp/test.php
@@ -1,12 +1,6 @@
 $course = Course::find($id);
-$course->name = 'Testveranstaltung 2';
-$changed = $course->store();
-
-$data = array('name' => 'Testveranstaltung 2', 'nummer' => '12345');
-$course->setData($data);
-
-$data['members'][] = array('status' => 'dozent', 'id' => 5);
-
-$course = Course::import($data);
-$course->store(); 
-//speichert neuen Kurs mit einem zugewiesenen Dozenten
+$course->delete();
+if ($course->isDeleted()) {
+  $course->setNew();
+  $course->store();
+}
