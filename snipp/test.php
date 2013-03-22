e37f453cc2e7d1e1fff96c0a589c63728da1ba5b
André Noack <noackorama@googlemail.com>
2013-03-22 01:11:24 +0100

    Update test.php

--- snipp/test.php
+++ snipp/test.php
@@ -1,13 +1,31 @@
-class Course extends SimpleORMap
-{
-    function __construct($id = null)
-    {
-        $this->db_table = 'seminare';
-        $this->default_values['admission_endtime'] = -1;
-        $this->alias_fields['number'] = 'veranstaltungsnummer';
-        $this->additional_fields['end_time']['get'] = function($course) {
-            return $course->duration_time == -1 ? -1 : $course->start_time + $course->duration_time;
-        };
-        parent::__construct($id);
-    }
-}
+
+        $this->has_many = array(
+                'members' => array(
+                        'class_name' => 'CourseMember',
+                        'on_delete' => 'delete', 'on_store' => 'store'),
+        );
+        $this->belongs_to = array(
+                'start_semester' => array(
+                        'class_name' => 'Semester',
+                        'foreign_key' => 'start_time',
+                        'assoc_func' => 'findByTimestamp',
+                        'assoc_foreign_key' => 'beginn'),
+                'end_semester' => array(
+                        'class_name' => 'Semester',
+                        'foreign_key' => 'end_time',
+                        'assoc_func' => 'findByTimestamp',
+                        'assoc_foreign_key' => 'beginn'),
+                'home_institut' => array(
+                        'class_name' => 'Institute',
+                        'foreign_key' => 'institut_id',
+                        'assoc_func' => 'find')
+        );
+        $this->has_and_belongs_to_many = array(
+                'study_areas' => array(
+                        'class_name' => 'StudipStudyArea',
+                        'thru_table' => 'seminar_sem_tree',
+                        'on_delete' => 'delete', 'on_store' => 'store'),
+                'institutes' => array(
+                        'class_name' => 'Institute',
+                        'thru_table' => 'seminar_inst',
+                        'on_delete' => 'delete', 'on_store' => 'store'));
