d4cfdf4a308ff058184a56614621efc6a8237b7c
André Noack <noackorama@googlemail.com>
2013-03-22 01:24:36 +0100

    Update test.php

--- snipp/test.php
+++ snipp/test.php
@@ -1,31 +1,5 @@
-
-        $this->has_many = array(
-                'members' => array(
-                        'class_name' => 'CourseMember',
-                        'on_delete' => 'delete', 'on_store' => 'store'),
-        );
-        $this->belongs_to = array(
-                'start_semester' => array(
-                        'class_name' => 'Semester',
-                        'foreign_key' => 'start_time',
-                        'assoc_func' => 'findByTimestamp',
-                        'assoc_foreign_key' => 'beginn'),
-                'end_semester' => array(
-                        'class_name' => 'Semester',
-                        'foreign_key' => 'end_time',
-                        'assoc_func' => 'findByTimestamp',
-                        'assoc_foreign_key' => 'beginn'),
-                'home_institut' => array(
-                        'class_name' => 'Institute',
-                        'foreign_key' => 'institut_id',
-                        'assoc_func' => 'find')
-        );
-        $this->has_and_belongs_to_many = array(
-                'study_areas' => array(
-                        'class_name' => 'StudipStudyArea',
-                        'thru_table' => 'seminar_sem_tree',
-                        'on_delete' => 'delete', 'on_store' => 'store'),
-                'institutes' => array(
-                        'class_name' => 'Institute',
-                        'thru_table' => 'seminar_inst',
-                        'on_delete' => 'delete', 'on_store' => 'store'));
+if ($this->hasAutoIncrementColumn()) {
+    $this->registerCallback('before_store after_create', 'cbAutoIncrementColumn');
+} elseif (count($this->pk) === 1) {
+    $this->registerCallback('before_store', 'cbAutoKeyCreation');
+}
